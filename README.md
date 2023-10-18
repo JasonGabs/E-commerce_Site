f1fc0ecd0a6192fb3dd6d21f3c22931567fc925c
JasonGabs <jasongabs@gmail.com>
2023-10-18 13:44:05 -0700

    Added product routes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # E-commerce_Site
+An E-commerce site
--- models/index.js
+++ models/index.js
@@ -4,13 +4,14 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-
-// Categories have many Products
-
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
+// product in in category
+Product.belongsTo(Category, { foreignKey: "category_id" });
+// category can have many products
+Category.hasMany(Product, { foreignKey: "category_id" });
+// product has many tags
+Product.belongsToMany(Tag, { through: ProductTag, foreignKey: "product_id" });
+// tags can have many products
+Tag.belongsToMany(Product, { through: ProductTag, foreignKey: "tag_id"});
 
 module.exports = {
   Product,
